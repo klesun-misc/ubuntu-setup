134d563125b9bed5f9f49228f81ffc11593f4431
Artur Klesun <30558426+klesun-misc@users.noreply.github.com>
2017-07-29 23:54:50 +0300

    Create ubuntu-setup.sh

---
+++ ubuntu-setup.sh
@@ -0,0 +1,9 @@
+# run this script on a fresh xubuntu installation to install must-have apps
+sudo apt-get update
+sudo apt-get install vim geany clipit guake git sshfs openssh-server baobab chromium-browser
+
+# manual steps follow...
+# - In "Settings -> Keyboard" set repeat delay to 120 and speed to 200
+# - In guake settings set shortcut to F10 to not conflict with JS console shortcut
+# - In clipit settings set shortcut to Alt+H instead of Ctrl+Alt+H
+# - Download `bin` folder from this repository to your home directory (or make a simlink called "bin" in your home directory)
