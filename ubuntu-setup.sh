d3c6a280408ee45cdd3889fe4238ded727416d7d
Artur Klesun <30558426+klesun-misc@users.noreply.github.com>
2017-07-30 22:12:41 +0300

    Add synergy

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -2,6 +2,10 @@
 sudo apt-get install vim geany clipit guake git sshfs openssh-server baobab chromium-browser python3-pip openjdk-8-jdk
 rm -R ~/Videos/ ~/Public/ ~/Templates/ ~/Pictures/ ~/Music/ ~/Documents/
 
+wget https://github.com/brahma-dev/synergy-stable-builds/releases/download/v1.8.8-stable/synergy-v1.8.8-stable-Linux-i686.deb
+sudo dpkg -i synergy-v1.8.8-stable-Linux-i686.deb
+rm synergy-v1.8.8-stable-Linux-i686.deb
+
 mkdir ~/big
 mkdir ~/big/gits
 git clone https://github.com/klesun-misc/ubuntu-setup/ ~/big/gits/ubuntu-setup
