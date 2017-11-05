9f93845804021a675d24c39f4207611c72d6cb7f
Artur Klesun <arturklesun@gmail.com>
2017-11-05 23:32:16 +0000

    Add apt-get update to the setup script

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,4 +1,5 @@
 # run this script on a fresh xubuntu installation to install must-have apps
+sudo apt-get update
 sudo apt-get install vim geany clipit guake git sshfs openssh-server baobab chromium-browser python3-pip openjdk-8-jdk gimp vlc samba
 rm -R ~/Videos/ ~/Public/ ~/Templates/ ~/Pictures/ ~/Music/ ~/Documents/
 
